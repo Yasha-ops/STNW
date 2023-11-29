0a318f0f3805b7172fa6362c271ef5e6880942c1
Yassine Damiri <yassinedamiri007@gmail.com>
2023-11-29 01:34:20 +0100

    Moved index to project path

---
+++ index.ts
@@ -0,0 +1,16 @@
+import NmapScanner, {
+  QuickScanResult,
+  PortScanResult,
+  HostUpResult,
+  OsDetectionResult,
+  ServiceScanResult,
+} from "./src/nmap";
+
+export default NmapScanner;
+export {
+  QuickScanResult,
+  PortScanResult,
+  HostUpResult,
+  OsDetectionResult,
+  ServiceScanResult,
+};
